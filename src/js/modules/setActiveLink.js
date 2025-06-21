c037eef3ce801d9ea5c31482f5d1c01198294226
Alex Levchuk <alexlevchuk@mail.ru>
2025-06-21 10:43:41 +0300

    add order base structure

--- src/js/modules/setActiveLink.js
+++ src/js/modules/setActiveLink.js
@@ -6,22 +6,24 @@ function setActiveLink() {
 	const headerMenuItems = document.querySelectorAll('.header__menu-item');
 	const currentPath = window.location.href;
 
-	console.log(currentPath);
-
 	headerMenuItems.forEach((item) => {
-		const itemPath = new URL(item.href).href;
+		if (item) {
+			const itemPath = new URL(item.href).href;
 
-		if (currentPath === itemPath) {
-			item.classList.add('active');
-		} else {
-			item.classList.remove('active');
+			if (currentPath === itemPath) {
+				item.classList.add('active');
+			} else {
+				item.classList.remove('active');
+			}
 		}
 	});
 
 	headerMenuItems.forEach((item) => {
-		item.addEventListener('click', () => {
-			headerMenuItems.forEach((el) => el.classList.remove('active'));
-			item.classList.add('active');
-		});
+		if (item) {
+			item.addEventListener('click', () => {
+				headerMenuItems.forEach((el) => el.classList.remove('active'));
+				item.classList.add('active');
+			});
+		}
 	});
 }
